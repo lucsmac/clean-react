679e274ecd168cf81ca1b5cb87f2f82798b46592
Lucas Macedo <lucasmdc57@gmail.com>
2022-03-15 11:06:54 -0300

    refactor: move HttpPostClientSpy to its own file

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient } from './protocols/http/http-post-client'
+import { HttpPostClient } from '../../protocols/http/http-post-client'
 
 export class RemoteAuthentication {
   constructor(
@@ -10,4 +10,3 @@ export class RemoteAuthentication {
     await this.httpPostClient.post(this.url)
   }
 }
-
