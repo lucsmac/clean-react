80d70593c4e4778e572723f0aa41e9487b280829
Lucas Macedo <lucasmdc57@gmail.com>
2022-03-27 18:23:04 -0300

    refactor: change validation interface

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
 import Login from './login'
-import { Validation } from '@/presentation/protocols/validation'
+import { ValidationSpy } from '@/presentation/test'
 
 type SutTypes = {
   sut: RenderResult
   validationSpy: ValidationSpy
-}
-
-class ValidationSpy implements Validation {
-  errorMessage: string
-  input: object
-
-  validate(input: object): string {
-    this.input = input
-    return this.errorMessage
-  }
 }
 
 const makeSut = (): SutTypes => {
@@ -58,18 +48,16 @@ describe('Login Component', () => {
       const { sut, validationSpy } = makeSut()
       const emailInput = sut.getByTestId('email')
       fireEvent.input(emailInput, { target: { value: 'any_email' } })
-      expect(validationSpy.input).toEqual({
-        email: 'any_email'
-      })
+      expect(validationSpy.fieldName).toBe('email')
+      expect(validationSpy.fieldValue).toBe('any_email')
     })
 
     test('With correct password', () => {
       const { sut, validationSpy } = makeSut()
       const passwordInput = sut.getByTestId('password')
       fireEvent.input(passwordInput, { target: { value: 'any_password' } })
-      expect(validationSpy.input).toEqual({
-        password: 'any_password'
-      })
+      expect(validationSpy.fieldName).toBe('password')
+      expect(validationSpy.fieldValue).toBe('any_password')
     })
   })
 })
