83b025aa026ff0d8b407b35e086679cf9bb9167f
Lucas Macedo <lucasmdc57@gmail.com>
2022-06-06 11:48:07 -0300

    test: ensure ValidationComposite returns first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -15,4 +15,19 @@ describe('ValidationComposite', () => {
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('any_error_message')
   })
+
+  test('Should return first error if multiple validations fails', () => {
+    const validationSpy = new FieldValidationSpy('any_field')
+    validationSpy.error = new Error('first_error_message')
+    const validationSpy2 = new FieldValidationSpy('any_field')
+    validationSpy2.error = new Error('second_error_message')
+
+    const sut = new ValidationComposite([
+      validationSpy,
+      validationSpy2
+    ])
+
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('first_error_message')
+  })
 })
