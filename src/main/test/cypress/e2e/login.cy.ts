c7c30f9b03d025f670c5650af0fd7ef1f94a6b66
Lucas Macedo <lucasmdc57@gmail.com>
2022-06-13 21:53:51 -0300

    test: ensure Login save accessToken if valid credentials are provided

--- src/main/test/cypress/e2e/login.cy.ts
+++ src/main/test/cypress/e2e/login.cy.ts
@@ -41,18 +41,16 @@ describe('Login', () => {
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('email-status')
       .should('not.have.attr', 'title')
-    // .should('contain.text', '✔')
 
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('password-status')
       .should('not.have.attr', 'title')
-    // .should('contain.text', '✔')
 
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
-  it('should present valid state if form is valid', () => {
+  it('should present error if invalid credentials are provided', () => {
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('submit').click()
@@ -62,5 +60,17 @@ describe('Login', () => {
       .getByTestId('spinner').should('not.exist')
       .getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente.')
     cy.url().should('eq', `${baseUrl}/login`)
+  })
+
+  it('should save accessToken if valid credentials are provided', () => {
+    cy.getByTestId('email').focus().type('mango@gmail.com')
+    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+    cy.url().should('eq', `${baseUrl}/`)
+    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
 });
