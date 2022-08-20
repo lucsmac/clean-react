05708fefda041d54c0c6789e9782508fea9cf1d5
Lucas Macedo <lucasmdc57@gmail.com>
2022-08-20 09:11:59 -0300

    test: ensure RemoteLoadSurveyList returns a list on 200

---
+++ src/domain/test/mock-survey-list.ts
@@ -0,0 +1,18 @@
+import { SurveyModel } from '@/domain/models'
+import faker from 'faker'
+
+export const mockSurveyListModel = (): SurveyModel[] => ([
+  {
+    id: faker.random.uuid(),
+    question: faker.random.words(10),
+    answers: [{
+      answer: faker.random.words(4),
+      image: faker.internet.url()
+    }, {
+      answer: faker.random.words(4),
+      image: faker.internet.url()
+    }],
+    didAnswer: faker.random.boolean(),
+    date: faker.date.recent()
+  }
+])
