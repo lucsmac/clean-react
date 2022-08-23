c7dab7d32302f60e158c3ce370cbe368cc0dcbcb
Lucas Macedo <lucasmdc57@gmail.com>
2022-08-23 08:03:55 -0300

    feat: ensure SurveyList calls LoadSurveyList

---
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -0,0 +1,40 @@
+import { SurveyList } from '@/presentation/pages'
+import { render, screen } from "@testing-library/react"
+import React from 'react'
+import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
+
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+
+  async loadAll(): Promise<SurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy();
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+
+  return {
+    loadSurveyListSpy
+  }
+}
+
+describe('SurveyList Component', () => {
+  test('Should present 4 empty items on start', () => {
+    makeSut()
+    const surveyList = screen.getByTestId('survey-list')
+    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  test('Should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
+  })
+})
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,16 +1,27 @@
 import { Footer, Header } from '@/presentation/components'
-import React from 'react'
-import SurveyIitem from './components/survey-item/survey-item'
+import React, { useEffect } from 'react'
+import { SurveyItemEmpty } from '@/presentation/pages/survey-list/components'
 import Styles from './survey-list-styles.scss'
+import { LoadSurveyList } from '@/domain/usecases'
 
-const SurveyList: React.FC = () => {
+type Props = {
+  loadSurveyList: LoadSurveyList
+}
+
+const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  useEffect(() => {
+    (async function () {
+      loadSurveyList.loadAll()
+    })()
+  }, [])
+
   return (
-    <div className={Styles.surveyListWrap}>
+    <div className={Styles.surveyListWrap} data-testid="survey-list">
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul>
-          <SurveyIitem />
+          <SurveyItemEmpty />
         </ul>
       </div>
       <Footer />
